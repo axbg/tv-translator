355e7c7a383be7c9cc7a59949b3704f5ff93b9a9
Alex Bisag <bisagalexstefan@gmail.com>
2019-05-01 23:07:23 +0300

    Added TV_Translator.

---
+++ TV_Translator/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace TV_Translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TV Translator";
+            // 
+            // tbFile
+            // 
+            this.tbFile.Location = new System.Drawing.Point(69, 93);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.ReadOnly = true;
+            this.tbFile.Size = new System.Drawing.Size(283, 22);
+            this.tbFile.TabIndex = 0;
+            this.tbFile.Text = "Choose a file";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(213, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(433, 209);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbFile);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TV Translator";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TV_Translator/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TV_Translator
+{
+    public partial class Form1 : Form
+    {
+        String filename;
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            this.ActiveControl = button1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.Filter = "(*.srt, *.txt)|*.srt;*.txt";
+
+            if(openDlg.ShowDialog() == DialogResult.OK)
+            {
+                int length = openDlg.FileName.Split('\\').Length;
+                tbFile.Text = openDlg.FileName.Split('\\')[length - 1];
+                this.filename = openDlg.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(this.filename,FileMode.Open,FileAccess.Read);
+            StreamReader sr = new StreamReader(fs,Encoding.Default,true);
+            String content = sr.ReadToEnd();
+
+            fs.Close();
+            sr.Close();
+
+            content = content.Replace('þ', 'ț');
+            content = content.Replace('º', 'ș');
+            content = content.Replace('Þ', 'Ț');
+            content = content.Replace('ª', 'Ș');
+
+            FileStream sfs = new FileStream(this.filename, FileMode.Open, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(sfs,Encoding.UTF8);
+        
+            sw.Write(content);
+
+            sw.Close();
+            sfs.Close();
+
+            MessageBox.Show("Subtitle was corrected!");
+        }
+    }
+}
